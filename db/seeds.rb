5edb6ca2984ff5d1e70a596307a557d367025112
Nash <jaifowsar@Jais-MBP-2.westell.com>
2016-03-31 00:45:29 -0500

    api_auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-5.times do
-  User.create! username: Faker::Name.first_name,
-               password_digest: Faker::Number.digit
-end
-
 20.times do
   Post.create! title: Faker::Hacker.say_something_smart
 
